822bdd23d7bff41e2a6b8f0da1f226a0a505bfca
Illia Losiei <t0955037700@gmail.com>
2024-07-06 23:38:35 +0300

    Homework#22

--- api-tests/src/test/java/com/practicesoftwaretesting/BaseTest.java
+++ api-tests/src/test/java/com/practicesoftwaretesting/BaseTest.java
@@ -1,88 +1,36 @@
 package com.practicesoftwaretesting;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.github.javafaker.Faker;
-import com.practicesoftwaretesting.user.UserController;
-import com.practicesoftwaretesting.user.model.LoginRequest;
+import com.practicesoftwaretesting.user.UserSteps;
 import com.practicesoftwaretesting.user.model.RegisterUserRequest;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
+
+import static com.practicesoftwaretesting.user.UserSteps.ADMIN_EMAIL;
+import static com.practicesoftwaretesting.user.UserSteps.ADMIN_PASSWORD;
 
 public abstract class BaseTest {
 
-    protected static final String DEFAULT_PASSWORD = "Super-secret211@@";
-    private static final String ADMIN_EMAIL = "admin@practicesoftwaretesting.com";
-    private static final String ADMIN_PASSWORD = "welcome01";
-
-    static {
-        configureRestAssured();
-        RestAssured.requestSpecification = new RequestSpecBuilder()
-                .log(LogDetail.ALL)
-                .build();
-        RestAssured.responseSpecification = new ResponseSpecBuilder()
-                .log(LogDetail.ALL)
-                .build();
-    }
-
-    private static void configureRestAssured() {
-        var objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-
-        RestAssured.config = RestAssured.config()
-                .objectMapperConfig(
-                        RestAssured.config()
-                                .getObjectMapperConfig()
-                                .jackson2ObjectMapperFactory((cls, charset) -> objectMapper)
-                );
-    }
+    UserSteps userSteps = new UserSteps();
 
     public void registerUser(String userEmail, String userPassword) {
-        var userController = new UserController();
-        var registerUserRequest = buildUser(userEmail, userPassword);
-        userController.registerUser(registerUserRequest).as();
+        userSteps.registerUser(userEmail, userPassword);
     }
 
     public String loginUser(String userEmail, String userPassword) {
-        var userController = new UserController();
-        var userLoginResponse = userController.loginUser(new LoginRequest(userEmail, userPassword))
-                .as();
-        return userLoginResponse.getAccessToken();
+        return userSteps.loginUser(userEmail, userPassword);
     }
 
     public String registerAndLoginNewUser() {
-        var userEmail = getUserEmail();
-        registerUser(userEmail, DEFAULT_PASSWORD);
-        return loginUser(userEmail, DEFAULT_PASSWORD);
+        return userSteps.registerAndLoginNewUser();
     }
 
     public String loginAsAdmin() {
         return loginUser(ADMIN_EMAIL, ADMIN_PASSWORD);
     }
 
-    protected RegisterUserRequest buildUser(String email, String password) {
-        return RegisterUserRequest.builder()
-                .firstName("John")
-                .lastName("Lucas")
-                .address("Street 1")
-                .city("City")
-                .state("State")
-                .country("Country")
-                .postcode("1234AA")
-                .phone("0987654321")
-                .dob("1970-01-01")
-                .password(password)
-                .email(email)
-                .build();
+    public RegisterUserRequest buildUser(String email, String password) {
+        return userSteps.buildUser(email, password);
     }
 
-    protected String getUserEmail() {
-        return Faker.instance()
-                .friends()
-                .character()
-                .toLowerCase()
-                .replaceAll(" ", "") + "@gmail.com";
+    public String getUserEmail() {
+        return userSteps.getUserEmail();
     }
 }
--- ui-tests/src/test/java/com/practicesoftwaretesting/UserTest.java
+++ ui-tests/src/test/java/com/practicesoftwaretesting/UserTest.java
@@ -1,9 +1,7 @@
 package com.practicesoftwaretesting;
 
-import com.practicesoftwaretesting.pages.Header;
-import com.practicesoftwaretesting.pages.HomePage;
-import com.practicesoftwaretesting.pages.LoginPage;
-import com.practicesoftwaretesting.pages.RegisterPage;
+import com.practicesoftwaretesting.pages.*;
+import com.practicesoftwaretesting.user.model.RegisterUserRequest;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -14,10 +12,11 @@ public class UserTest {
     Header header = new Header();
     LoginPage loginPage = new LoginPage();
     RegisterPage registerPage = new RegisterPage();
+    AccountPage accountPage = new AccountPage();
 
     @Test
     void registerNewUserAndLogin() {
-        open("https://practicesoftwaretesting.com/#");
+        open("https://practicesoftwaretesting.com");
         homePage.isLoaded();
         header.clickSignInMenuItem();
         loginPage.isLoaded()
@@ -25,5 +24,30 @@ public class UserTest {
         registerPage.isLoaded()
                 .assertThat()
                 .hasCorrectInfo();
+
+        var user = getUser();
+        registerPage.registerNewUser(user);
+
+        loginPage.isLoaded()
+                .login(user.getEmail(), user.getPassword());
+
+        accountPage.isLoaded();
+        header.assertThat().isSignedIn(user.getFirstName() + " " + user.getLastName());
+    }
+
+    private RegisterUserRequest getUser() {
+        return RegisterUserRequest.builder()
+                .firstName("George")
+                .lastName("Washington")
+                .address("326 Good Street")
+                .city("Illinoise")
+                .state("Arcanzas")
+                .country("Uruguay")
+                .postcode("54321")
+                .phone("12345678")
+                .dob("09/03/1895")
+                .email("hi887@gmail.com")
+                .password("123#HalloWorld")
+                .build();
     }
 }
